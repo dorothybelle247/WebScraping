e3e6e5b81da43740484f44d2d6e37ee2a11ee80e
Dorothy Ellaine Arman Prince <44729364+dorothybelle247@users.noreply.github.com>
2021-08-11 02:18:34 +0000

    spider crawl

---
+++ postcrawl/postcrawl/spiders/posy_spider.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class PoseSpider(scrapy.spider):
+    name = "post"
+
+    start_urls = [
+        "https://freshchef.online/about/",
+        "https://freshchef.online/order-now/"
+    ]
+
+    def parse(self, response):
+        page = response.url.split('/')[-1]
+        filename = "post-%s.html" % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+
